cd2b1cce29d472d7bd265875abafd48836daa741
opilane <opilane@HP-CZC0359NP1>
2024-02-07 11:04:15 +0200

    Add project files.

---
+++ SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -0,0 +1,33 @@
+
+namespace SolarPlanets.Views;
+
+public partial class PlanetsPage : ContentPage
+{
+    private const uint AnimationDuration = 800u;
+
+
+    public PlanetsPage()
+    {
+        InitializeComponent();
+    }
+
+    async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+    {
+        await CloseMenu();
+    }
+
+    private async Task CloseMenu()
+    {
+        //close menu and show again main content
+        _ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+        await MainContentGrid.TranslateTo(0, 0, AnimationDuration, Easing.CubicIn);
+    }
+
+    async void ProfilePicture_Clicked(System.Object sender, System.EventArgs e)
+    {
+        _ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+        await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+        _ = MainContentGrid.FadeTo(0.8, AnimationDuration);
+    }
+}
